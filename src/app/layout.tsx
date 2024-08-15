5cae38cf14ff3965ebb51d576a0667699a53a566
Ammar Ahmed <ammar7298@gmail.com>
2024-08-15 11:45:21 +0500

    hello

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import AroraBackground from "@/components/AroraBackground";
-import Cards from "@/components/Cards";
-import Cards3 from "@/components/Cards3";
-import LandingPage from "@/components/LandingPage";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,8 +20,6 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
       </head>
       <body className={inter.className}>
-        {/* <LandingPage />
-        <Cards3 /> */}
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Cards3 from "@/components/Cards3";
-import LandingPage from "@/components/LandingPage";
 
 export default function Home() {
   return (
-  <>
-  {/* <LandingPage/> */}
-  <Cards3/>
-  </>
+    <>
+      <Cards3 />
+    </>
   );
 }
