2297f01c3f3891a307e5567bd37bd023f0922f73
Ammar Ahmed <ammar7298@gmail.com>
2024-08-15 14:31:17 +0500

    bass

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -20,7 +20,7 @@ const LandingPage = () => {
       <section className="home" id="Home">
         <div className="content">
           <h1>
-            I am<span>Hasham Rana</span>
+            I am <span>Hasham Rana</span>
           </h1>
           <h3 className="text-animation">
             I specialize in <span></span>
