09eecb1416fb60938e1e24867cac497c8e5a0d6f
Ammar Ahmed <ammar7298@gmail.com>
2024-08-15 12:51:56 +0500

    evert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Cards3 from "@/components/Cards3";
+import LandingPage from "@/components/LandingPage";
 
 export default function Home() {
   return (
     <>
+      <LandingPage/>
       <Cards3 />
     </>
   );
